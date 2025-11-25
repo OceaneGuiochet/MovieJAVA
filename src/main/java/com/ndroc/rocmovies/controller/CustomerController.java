bbf33757963bafe27e6407234bf4aaba1da53f6d
OceaneGuiochet <135025621+OceaneGuiochet@users.noreply.github.com>
2025-11-25 16:56:31 +0100

    add get customers by id

--- src/main/java/com/ndroc/rocmovies/controller/CustomerController.java
+++ src/main/java/com/ndroc/rocmovies/controller/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ndroc.rocmovies.entity.Customer;
@@ -18,5 +19,14 @@ public class CustomerController {
     @GetMapping("/customers")
     public List<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/customers/{id}")
+    public Customer getCustomerById(@PathVariable Integer id) {
+        return customerService.getAllCustomers()
+                .stream()
+                .filter(c -> c.getCustomerId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Customer not found"));
     }
 }
